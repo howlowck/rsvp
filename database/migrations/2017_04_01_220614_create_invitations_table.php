4b90897ffae9e09c3994625dc02120d2940b3245
Hao Luo <howlowck@gmail.com>
2017-04-01 22:31:47 -0500

    initial commit

---
+++ database/migrations/2017_04_01_220614_create_invitations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInvitationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invitations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('invite_code');
+            $table->boolean('plus_one');
+            $table->boolean('confirmed')->default(false);
+            $table->string('address_street');
+            $table->string('address_city');
+            $table->string('address_zipcode');
+            $table->text('notes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('invitations');
+    }
+}
