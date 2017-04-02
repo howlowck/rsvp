af963cdaa6a06876ddfc59bbece0b3e683289cd0
Hao Luo <howlowck@gmail.com>
2017-04-02 16:28:50 -0500

    changed composer install script

--- deploy.cmd
+++ deploy.cmd
@@ -63,14 +63,17 @@ IF EXIST "%DEPLOYMENT_SOURCE%\composer.json" (
 
   IF NOT EXIST "%DEPLOYMENT_SOURCE%\composer.phar" (
     echo Composer.phar not found. Downloading...
-    call curl -s https://getcomposer.org/installer | php
+    call php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+    call php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+    call php composer-setup.php
+    call php -r "unlink('composer-setup.php');"
     IF !ERRORLEVEL! NEQ 0 goto error
   ) ELSE (
       echo Attempting to update composer.phar
       call php composer.phar self-update
   )
 
-  echo "has composer.phar"
+  echo Has composer.phar now
 
   call php composer.phar install --no-dev
   IF !ERRORLEVEL! NEQ 0 goto error
