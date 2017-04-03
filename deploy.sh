778d8027480cea7cec7e2e538e581bff128324de
Hao Luo <howlowck@gmail.com>
2017-04-02 21:48:51 -0500

    simple composer install

--- deploy.sh
+++ deploy.sh
@@ -41,24 +41,14 @@ echo "target: $DEPLOYMENT_TARGET"
 
 if [ -e "$CURRENTDIR/composer.json" ]
 then
-  php -r "echo 'starting deployment!';"
+  echo "Start composer setup"
   if [ ! -e "$CURRENTDIR/composer.phar" ]
   then
-    EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
-    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-    ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
-
-    if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
-    then
-      >&2 echo 'ERROR: Invalid installer signature'
-      rm composer-setup.php
-      exit 1
-    fi
-
-    php composer-setup.php --quiet
-    RESULT=$?
-    rm composer-setup.php
+    echo "composer not found. Installing..."
+    curl -s https://getcomposer.org/installer | php
+    echo "composer is downloaded"
   else
+    echo "composer.phar found! updating..."
     php composer.phar self-update
   fi
 
